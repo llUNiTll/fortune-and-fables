250e6b4cb1648c99f54bccfa4e522147b5877f79
llUNiTll <d7npbvj68r@privaterelay.appleid.com>
2026-09-04 11:46:09 +0100

    Refresh offline app for purse loading fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fortune-fables-offline-v7';
+const CACHE='fortune-fables-offline-v8';
 const BASE=new URL(self.registration.scope).pathname.replace(/\/$/,'');
 const path=(value)=>`${BASE}${value}`;
 const SHELL=['/','/manifest.webmanifest','/app-icon.svg','/fortune-fables-mark.png','/assets/app.js','/assets/app.css'].map(path);
