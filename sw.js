05ff35751022e4dae9b7b824283628daeb1cae14
llUNiTll <d7npbvj68r@privaterelay.appleid.com>
2026-09-04 10:20:22 +0100

    Publish Fortune & Fables web app

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='fortune-fables-offline-v2';
+const BASE=new URL(self.registration.scope).pathname.replace(/\/$/,'');
+const path=(value)=>`${BASE}${value}`;
+const SHELL=['/','/manifest.webmanifest','/app-icon.svg','/fortune-fables-mark.png','/assets/app.js','/assets/app.css'].map(path);
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match(path('/')))));
+});
